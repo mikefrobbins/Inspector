d41ca4770cb917f90d1fde3d76b620758f8d0fe2
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2023-11-17 07:36:56 -0600

    Added error handling and comment-based help

--- MrInspector/public/Get-MrMandatoryParameter.ps1
+++ MrInspector/public/Get-MrMandatoryParameter.ps1
@@ -1,12 +1,44 @@
 ﻿function Get-MrMandatoryParameter {
 
+<#
+.SYNOPSIS
+    Retrieves mandatory parameters for a specified PowerShell command.
+
+.DESCRIPTION
+    The Get-MrMandatoryParameter function fetches all the mandatory parameters
+    of a given PowerShell command. This can be helpful for understanding the
+    required inputs for using the command effectively.
+
+.PARAMETER Name
+    The name of the PowerShell command for which mandatory parameters are to be retrieved.
+
+.EXAMPLE
+    Get-MrMandatoryParameter -Name Set-Location
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com/
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
         [string]$Name
     )
 
-    Get-MrParameterSet -Name $Name |
-    Where-Object Mandatory -eq $true
+    try {
+        $commandParamSets = Get-MrParameterSet -Name $Name -ErrorAction Stop
+    } catch {
+        Write-Error "Command '$Name' not found or error in Get-MrParameterSet."
+        return
+    }
 
+    $mandatoryParams = $commandParamSets | Where-Object Mandatory -eq $true
+
+    if ($mandatoryParams) {
+        Write-Output $mandatoryParams
+    } else {
+        Write-Verbose "No mandatory parameters found for command '$Name'."
+    }
 }
