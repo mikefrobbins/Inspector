00781540258ca9ee353acb40a76e8a3c53b9f68d
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2023-11-19 06:09:04 -0600

    Refactored Get-MrParameterSet

--- MrInspector/public/Get-MrParameterSet.ps1
+++ MrInspector/public/Get-MrParameterSet.ps1
@@ -1,69 +1,62 @@
 ﻿#Requires -Version 4.0
 function Get-MrParameterSet {
 
-<#
-.SYNOPSIS
-    List parameters by parameter sets, parameter aliases, and whether parameters are mandatory.
+    <#
+    .SYNOPSIS
+        Lists parameters by parameter sets, parameter aliases, and whether parameters are mandatory.
 
-.DESCRIPTION
-    Get-MrParameterSet is a PowerShell function that lists detailed information about a command's parameter set including parameter set names, parameter names, parameter aliases, and if parameters are mandatory.
+    .DESCRIPTION
+        Get-MrParameterSet is a PowerShell function that lists detailed information about a command's
+        parameter sets including parameter set names, parameter names, parameter aliases, and if
+        parameters are mandatory.
 
-.PARAMETER Name
-    Name of one or more PowerShell commands.
+    .PARAMETER Name
+        Name of one or more PowerShell commands.
 
-.EXAMPLE
-    Get-MrParameterSet -Name Get-Alias, Get-Command
+    .EXAMPLE
+        Get-MrParameterSet -Name Get-Alias, Get-Command
 
-.EXAMPLE
-    'Get-Alias', 'Get-Command' | Get-MrParameterSet
+    .EXAMPLE
+        'Get-Alias', 'Get-Command' | Get-MrParameterSet
 
-.NOTES
-    Author:  Mike F Robbins
-    Website: https://mikefrobbins.com
-    Twitter: @mikefrobbins
-#>
+    .NOTES
+        Author:  Mike F. Robbins
+        Website: https://mikefrobbins.com/
+        Twitter: @mikefrobbins
+    #>
 
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory,
-                   ValueFromPipeline)]
-        [string[]]$Name
-    )
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory, ValueFromPipeline)]
+            [string[]]$Name
+        )
 
-    PROCESS {
+        PROCESS {
+            foreach ($n in $Name) {
+                $cmdlet = Get-Command -Name $n -ErrorAction SilentlyContinue
 
-        foreach ($n in $Name) {
-
-            $Cmdlet = Get-Command -Name $n
-
-            if ($Cmdlet.CommandType -eq 'Alias') {
-                $Cmdlet = Get-Command -Name $Cmdlet.ResolvedCommand
-            }
-
-            $ParameterSets = $Cmdlet.ParameterSets.Name
-
-            foreach ($ParameterSet in $ParameterSets){
-
-                $Parameters = $Cmdlet.ParameterSets.Where({$_.Name -eq $ParameterSet}).Parameters
-
-                foreach ($Parameter in $Parameters){
-
-                    [pscustomobject]@{
-                        Name = $n
-                        CmdletName = $Cmdlet.Name
-                        ParameterSet = $ParameterSet
-                        Parameter = $Parameter.Name
-                        Alias = $Parameter.Aliases
-                        Mandatory = $Parameter.IsMandatory
-                        PSTypeName = 'Mr.GetParameterSet'
-                    }
-
+                if ($null -eq $cmdlet) {
+                    Write-Warning "Command '$n' not found."
+                    continue
                 }
 
+                if ($cmdlet.CommandType -eq 'Alias') {
+                    $cmdlet = Get-Command -Name $cmdlet.ResolvedCommand
+                }
+
+                foreach ($parameterSet in $cmdlet.ParameterSets) {
+                    foreach ($parameter in $parameterSet.Parameters) {
+                        [pscustomobject]@{
+                            Name = $n
+                            CmdletName = $Cmdlet.Name
+                            ParameterSet = $ParameterSet.Name
+                            Parameter = $Parameter.Name
+                            Alias = $Parameter.Aliases -join ', '
+                            Mandatory = $Parameter.IsMandatory
+                            PSTypeName = 'Mr.GetParameterSet'
+                        }
+                    }
+                }
             }
-
         }
-
     }
-
-}
