bf459bcb95805bed9821509885c1432641629dac
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2023-11-21 05:40:47 -0600

    Refactored and added comment-based help

--- MrInspector/public/Resolve-MrParameterSet.ps1
+++ MrInspector/public/Resolve-MrParameterSet.ps1
@@ -1,28 +1,75 @@
-﻿function Resolve-MrParameterSet {
+﻿#Requires -Version 3.0
+function Resolve-MrParameterSet {
+
+<#
+.SYNOPSIS
+Resolves and returns the parameter set currently in use for a given PowerShell cmdlet and a set of parameters.
+
+.DESCRIPTION
+The Resolve-MrParameterSet function analyzes one or more PowerShell cmdlets and determines which parameter set is being used based on the specified parameters. It resolves any parameter aliases and finds the matching parameter set for each cmdlet, returning a custom object containing the cmdlet name and the name of the parameter set in use. If no parameters match a specific set, the function defaults to the cmdlet's default parameter set.
+
+.PARAMETER Cmdlet
+Specifies one or more cmdlets to analyze. This parameter is mandatory and accepts an array of string values, each representing a cmdlet's name.
+
+.PARAMETER Parameter
+Specifies an array of parameters to resolve against the cmdlets' parameter sets. This parameter is optional.
+
+.EXAMPLE
+Resolve-MrParameterSet -Cmdlet Get-Process -Parameter Name
+
+.EXAMPLE
+Resolve-MrParameterSet -Cmdlet Get-Service, Get-Process -Parameter DisplayName, Id
+
+.NOTES
+    Author:  Mike F. Robbins
+    Website: https://mikefrobbins.com/
+    Twitter: @mikefrobbins
+
+#>
+
     [CmdletBinding()]
     param (
-        $Cmdlet,
-        $Parameter
+        [Parameter(Mandatory)]
+        [string[]]$Cmdlet,
+
+        [string[]]$Parameter
     )
 
-    foreach ($Command in $Cmdlet) {
-        $ParameterSets = Get-MrParameterSet -Name $Command
+    foreach ($command in $Cmdlet) {
+        $commandInfo = Get-Command -Name $command
+        $parameterSetsInfo = $commandInfo.ParameterSets |
+                            Select-Object -Property Name, @{label='Parameters';expression={$_.parameters.name}}
 
-        if (Get-MrParameterAlias -Name $Command | Where-Object aliases -eq $Parameter) {
-            $Parameter = (Get-MrParameterAlias -Name $Command | Where-Object aliases -eq $Parameter).Name
+        # Resolve parameter alias
+        $paramAliases = Get-MrParameterAlias -Name $command
+
+        $resolvedParameters = foreach ($param in $Parameter) {
+            $alias = $paramAliases | Where-Object Aliases -contains $param
+            if ($null -eq $alias) {
+            $param
+            } else {
+            $alias.Name
+            }
         }
 
-        $Results = foreach ($Param in $Parameter) {
-            if ($Param -in $ParameterSets.Parameter) {
-                $ParameterSets | Where-Object Parameter -eq $Param
+        # Determine the parameter set in use
+        $inUseParamterSet = $null
+
+        foreach ($set in $parameterSetsInfo) {
+            $isMatch = $resolvedParameters | ForEach-Object {$_ -in $set.Parameters}
+            if ($isMatch -notcontains $false) {
+                $inUseParamterSet = $set.Name
+                break
             }
+        }
+
+        if (-not $inUseParamterSet) {
+            $inUseParamterSet = $commandInfo.ParameterSets | Where-Object IsDefault -eq $true | Select-Object -First 1 -ExpandProperty Name
         }
 
         [pscustomobject]@{
             Command = $Command
-            ParameterSet = ($Results | Group-Object -Property ParameterSet | Sort-Object -Property Count -Descending | Select-Object -First 1).Name
+            ParameterSet = $inUseParamterSet
         }
-
     }
-
 }
